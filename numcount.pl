4b0c31a021c474ce7171092510d631631086c3bb
Lord <navaneethpradeep21@gmail.com>
2017-04-07 17:24:40 +0530

    Yo lord

---
+++ numcount.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+     
+my $filename = "numfile";
+my @count;
+open(my $fh, "<", $filename)
+     or die "Could not open '$filename': $!";
+     while (my $line = <$fh>) {
+        chomp $line;
+        my @chars = split //, $line;
+        foreach my $c (@chars) {
+            if ($c ne " ") {
+                $count[$c]++;
+            }
+        }
+    }
+foreach my $i (0..9) {
+        print "$i ", ($count[$i] ? $count[$i] : 0), "\n";
+    }
+     
